f26bab6338e914322499228accfc4e7cb97651cb
switch87 <gert@gepe-biljarts.be>
2015-10-12 20:16:15 +0200

    throw exception when key longer than 64 characters

--- gargoyle/manager.py
+++ gargoyle/manager.py
@@ -40,6 +40,11 @@ class SwitchManager(ModelDict):
 
         >>> gargoyle.is_active('my_feature', request) #doctest: +SKIP
         """
+
+        # check for varchar(64)
+        if len(key) > 64:
+            raise ValueError('Gargoyle key can be max 64 chars, was: %s' % len(key))
+
         default = kwargs.pop('default', False)
 
         # Check all parents for a disabled state
